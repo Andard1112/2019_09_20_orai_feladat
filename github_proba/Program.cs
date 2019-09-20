29c0ae998ee188ed63f6d8436b8099ed8708d886
Tordon <Tordon@DESKTOP-770IMGJ>
2019-09-20 15:37:46 +0200

    3.feladat

--- github_proba/Program.cs
+++ github_proba/Program.cs
@@ -9,17 +9,28 @@ namespace github_proba
             Console.WriteLine("Kérem adja meg, hogy milyen hosszú legyen a tomb");
             int meret = int.Parse(Console.ReadLine());
             int[] tomb = new int[meret];
-            int max = 0;
+            
             for (int i = 0; i < meret; i++)
             {
                 Console.WriteLine("Kérem adja meg "+ (i+1)+" számot: ");
                 tomb[i] = int.Parse(Console.ReadLine());
-                if (max< tomb[i])
+                
+            }
+            int max = tomb[0];
+            int min = tomb[0];
+            for (int i = 0; i < meret; i++)
+            {
+                if (max < tomb[i])
                 {
                     max = tomb[i];
                 }
+                if (min > tomb[i])
+                {
+                    min = tomb[i];
+                }
             }
             Console.WriteLine("A legnagyobb szám:{0}",max);
+            Console.WriteLine("A legkisebb szám:{0}", min);
             Console.ReadKey();
         }
     }
