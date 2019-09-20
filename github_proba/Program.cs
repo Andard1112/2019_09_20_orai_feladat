084cabf0203bfbdd7152d2ddcf678c320cacec68
Tordon <Tordon@DESKTOP-770IMGJ>
2019-09-20 15:15:04 +0200

    1. feladat

--- github_proba/Program.cs
+++ github_proba/Program.cs
@@ -6,7 +6,19 @@ namespace github_proba
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] tomb = new int[10];
+            int max = 0;
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                Console.WriteLine("Kérem adja meg "+ (i+1)+" számot: ");
+                tomb[i] = int.Parse(Console.ReadLine());
+                if (max< i)
+                {
+                    max = i;
+                }
+            }
+            Console.WriteLine("A legnagyobb szám:{0}",max);
+            Console.ReadKey();
         }
     }
 }
